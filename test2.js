8d23366cc72ee7f3eb81ba4adbd76718dea66aee
zhangchao <544262408@qq.com>
2021-11-02 17:17:37 +0800

    .

--- test2.js
+++ test2.js
@@ -6,6 +6,9 @@ var base64Img = require('base64-img');
 const random = require('random')
 const path = require('path')
 const FastAverageColor = require('fast-average-color-node');
+var load = require('./lib/load_images');
+var SSIM = require('image-ssim');
+// import ssim from "ssim.js";
 
 console.log(FastAverageColor)
 
@@ -24,7 +27,8 @@ async function Calculatecolor(){
     
 
     // const imglist = walkSync('img')
-    const imglist = ['Ringer_106.png','Ringer_110.png','Ringer_119.png','Ringer_122.png','Ringer_232.png']
+    const imglist = ['Ringer_106.png','Ringer_110.png','Ringer_119.png',
+    'Ringer_122.png','Ringer_232.png','Ringer_231.png']
     var imgcolor=[]
     
     for (let index = 0; index < imglist.length; index++) {
@@ -48,7 +52,8 @@ async function Calculatecolor(){
         itemimgColor=itemimgColor.value;
         console.log('比较',itemimgColor,imgcolor)
         var minimg = await findMinImg(itemimgColor,imgcolor)
-        console.log(minimg)
+        console.log('颜色相似的',minimg)
+        Structuralsimilarity(src,minimg);
         
         
    
@@ -80,7 +85,7 @@ async function findMinImg(imgColor,imgcolorlist){
         
     }
     minImg=minImg.sort(function(a, b){return a.num-b.num})
-    minImg=minImg.slice(0,4)
+    minImg=minImg.slice(0,10)
     console.log(minImg)
 
 
@@ -88,6 +93,47 @@ async function findMinImg(imgColor,imgcolorlist){
     return minImg;
 
 
+}
+
+async function Structuralsimilarity(imgfile,imgfileList){
+    
+    var  img1 = await loadImage(imgfile);
+    var  img2 = await loadImage('./img/'+imgfileList[0].img);
+    console.log( img1)
+    var  imgfile2='./img/'+imgfileList[0].img
+    console.log(load)
+    var img3 = await makeBigImg(img1,img2);
+
+    // var ssim = SSIM.compare(img3, img2);
+    // console.log(ssim)
+    imgfileList.forEach( async (imgpath)=>{
+        console.log('./img/'+imgpath.img)
+        var  img2 = await loadImage('./img/'+imgpath.img);
+        var img2_ = await makeBigImg(img2,img2)
+        // console.log(img3, img2)
+        var ssim = SSIM.compare(img3, img2_);
+        console.log(ssim,imgpath)
+    })
+
+}
+
+async function makeBigImg(img,imgtarget){
+
+    var num = Math.floor(6000/img.naturalWidth )
+    console.log('放大',num)
+    var wpiece = imgtarget.naturalWidth ;
+    var hpiece = imgtarget.naturalHeight ;
+    
+    var src = '';
+    
+    var canvas = createCanvas(wpiece, hpiece);
+    var ctx = canvas.getContext('2d');
+
+    canvas.width = wpiece;
+    canvas.height = hpiece;
+    ctx.drawImage(img, 0, 0, wpiece, hpiece)
+    var id = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    return {width: canvas.width, height: canvas.height, data: id.data, channels: 4}
 }
 
 Calculatecolor();
